b413d6ec16876375f7d767c8b321ff03928f01dc
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-12-19 23:50:45 +0700

    Add files via upload

---
+++ bot/config.js
@@ -0,0 +1,8 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+    consumer_key:         '4jB9BKPNqKSXqNObMsVTLu1Py',
+    consumer_secret:      'pqmDF5NbTlF36fXLw08BghLpXah3OBJMStnhYYxizOvj42Uvi8',
+    access_token:         '1427443084879532035-SCbT4yNYZGgx95ewPArE67mzkoKIbr',
+    access_token_secret:  'rsTnl0yZe17SmtEu2RgBrNFmeKthEQ2jDHdZ3Ov75dEez'
+  }
+  
